8e025f9f3571162f45a66244000db39da5516945
joekendal <13680617+joekendal@users.noreply.github.com>
2022-01-29 00:00:18 +0000

    fix: revert back to useState

--- src/contexts/Answers.tsx
+++ src/contexts/Answers.tsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import { consult } from 'nexusmed-js'
 import { AnswerInput } from 'nexusmed-js/dist/dts/consult/sdk'
-import useLocalStorageState from 'use-local-storage-state'
 
 export interface IAnswersContext {
   answers: Answers;
@@ -23,7 +22,7 @@ type Answers = {
 
 export const AnswersProvider: React.FC = ({ children }) => {
 
-  const [answers, _setAnswers] = useLocalStorageState<Answers>('nexus:qans', initialState.answers)
+  const [answers, _setAnswers] = useState<Answers>(initialState.answers)
 
   const setAnswer = (id: string, index: number, answer: AnswerInput) => {
     let ans = answers
